8c509c619754a0dae60ae7fb32702f1dff28b150
Allenghost <allen0901.tw@gmail.com>
2024-03-19 22:10:24 -0400

    Lab7

--- Allen Li - CGT215 - Lab7.cpp
+++ Allen Li - CGT215 - Lab7.cpp
@@ -2,10 +2,100 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+using namespace std;
+using namespace sf;
+using namespace sfp;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Create our window and world with gravity 0,1
+	RenderWindow window(VideoMode(800, 600), "Bounce");
+	World world(Vector2f(0, 1));
+	
+	// Create the ball
+	PhysicsCircle ball;
+	ball.setCenter(Vector2f(400, 400));
+	ball.setRadius(20);
+	world.AddPhysicsBody(ball);
+	ball.applyImpulse(Vector2f(0.7,0.7));
+	
+	// Create the floor
+	PhysicsRectangle floor;
+	floor.setSize(Vector2f(800, 20));
+	floor.setCenter(Vector2f(400, 590));
+	floor.setStatic(true);
+	world.AddPhysicsBody(floor); 
+	PhysicsRectangle ceiling;
+	ceiling.setSize(Vector2f(800, 20));
+	ceiling.setCenter(Vector2f(400, 10));
+	ceiling.setStatic(true);
+	world.AddPhysicsBody(ceiling);
+	PhysicsRectangle Lside;
+	Lside.setSize(Vector2f(20, 600));
+	Lside.setCenter(Vector2f(10, 300));
+	Lside.setStatic(true);
+	world.AddPhysicsBody(Lside);
+	PhysicsRectangle Rside;
+	Rside.setSize(Vector2f(20, 600));
+	Rside.setCenter(Vector2f(790, 300));
+	Rside.setStatic(true);
+	world.AddPhysicsBody(Rside);
+	PhysicsRectangle block;
+	block.setSize(Vector2f(100, 100));
+	block.setCenter(Vector2f(400, 300));
+	block.setStatic(true);
+	world.AddPhysicsBody(block);
+	
+	int thudCount(0);
+	floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+		};
+	ceiling.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+		};
+	Lside.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+		};
+	Rside.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+		};
+	
+	int bangCount(0);
+	block.onCollision = [&bangCount](PhysicsBodyCollisionResult result) {
+		cout << "bang " << bangCount << endl;
+		bangCount++;
+		if (bangCount >= 3) {
+			exit(0);
+		}
+		};
+	
+	
+	Clock clock;
+	Time lastTime(clock.getElapsedTime());
+	while (true) {
+		// calculate MS since last frame
+		Time currentTime(clock.getElapsedTime());
+		Time deltaTime(currentTime - lastTime);
+		int deltaTimeMS(deltaTime.asMilliseconds());
+		if (deltaTimeMS > 0) {
+			world.UpdatePhysics(deltaTimeMS);
+			lastTime = currentTime;
+		}
+		window.clear(Color(0, 0, 0));
+		window.draw(ball);
+		window.draw(floor);
+		window.draw(ceiling);
+		window.draw(Lside);
+		window.draw(Rside);
+		window.draw(block);
+		window.display();
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
